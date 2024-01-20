fdd066975edd8b41d6984fa44490915339641ef5
Ivan Beltrame <87547424+Imperatore1003@users.noreply.github.com>
2024-01-20 21:45:09 +0100

    Adding some words and attributing the source of the definitions

--- hangman-expert/scripts/word-list.js
+++ hangman-expert/scripts/word-list.js
@@ -1,15 +1,16 @@
+// Tutti gli indizi delle parole sono stati presi dal vocabolario Treccani https://www.treccani.it/ tranne specificazione diversa.
 let wordList = [
     {
         word: "elefantiaco",
-        hint: "Esageratamente complesso e macchinoso"
+        hint: "Esageratamente complesso e macchinoso" // Oxford Languages
     },
     {
         word: "grafomane",
-        hint: "Pazzo per la scrittura"
+        hint: "Pazzo per la scrittura" // https://it.wiktionary.org
     },
     {
         word: "coevo",
-        hint: "Contemporaneo a qualcuno o a qualcosa; della stessa epoca del nome a cui è riferito"
+        hint: "Che è vissuto nello stesso tempo, appartenente alla stessa età"
     },
     {
         word: "facinoroso",
@@ -17,11 +18,11 @@ let wordList = [
     },
     {
         word: "scatologico",
-        hint: "Che ha per argomento lo sterco"
+        hint: "Relativo a scritto o discorso che tratta di escrementi, o che comunque ha contenuto e tono osceno, volgare"
     },
     {
         word: "baruffa",
-        hint: "Alterco rumoroso fra più persone che finiscono per venire alle mani"
+        hint: "Zuffa confusa di persone che litigano e vengono alle mani"
     },
     {
         word: "chiozzotto",
@@ -30,5 +31,13 @@ let wordList = [
     {
         word: "flemma",
         hint: "Lentezza, calma, scarsa emotività"
+    },
+    {
+        word: "trascendentale",
+        hint: "Va oltre, supera certi limiti, un certo grado, un certo ordine"
+    },
+    {
+        word: "ascendentale",
+        hint: "Che va dal basso verso l'alto"
     }
 ];
--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const cacheVersion = "v2";
+const cacheVersion = "v3";
 const statiCache = "site-static-" + cacheVersion;
 const dynamicCache = "site-dynamic-" + cacheVersion;
 
